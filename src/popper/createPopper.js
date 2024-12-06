615b4d484fe9fccdaeebb186e9981676551f8a49
dev-javascript <javascript.code.dev@gmail.com>
2024-12-06 22:06:19 +0330

    fix style issue

--- src/popper/createPopper.js
+++ src/popper/createPopper.js
@@ -2,7 +2,7 @@ import {createPopper} from '@popperjs/core/lib/popper-lite';
 import flip from '@popperjs/core/lib/modifiers/flip';
 import preventOverflow from '@popperjs/core/lib/modifiers/preventOverflow';
 import computeStyles from '@popperjs/core/lib/modifiers/computeStyles';
-preventOverflow.options = {padding: 13, boundariesElement: 'viewport'};
+preventOverflow.options = {padding: 0, boundariesElement: 'viewport'};
 export default function (ref, popper, fullWidth, placement) {
   const op = {placement, strategy: 'fixed', modifiers: [flip, preventOverflow]};
   if (fullWidth == true) {
